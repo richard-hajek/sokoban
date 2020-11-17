e85cb713f937bfd630e577f619ee4087aa37c8fb
Richard Hajek <richard.m.hajek@gmail.com>
2020-11-17 14:01:30 +0100

    added deadsquaredetector

--- src/MyAgent.java
+++ src/MyAgent.java
@@ -1,11 +1,92 @@
 import static java.lang.System.out;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 import agents.ArtificialAgent;
 import game.actions.EDirection;
 import game.actions.compact.*;
 import game.board.compact.BoardCompact;
+import game.board.oop.EPlace;
+import game.board.oop.ESpace;
+
+class Pair<A, B>{
+	A x;
+	B y;
+
+	Pair(A a, B b){
+		x = a;
+		y = b;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Pair))
+			return false;
+
+		Pair q = (Pair) o;
+		return x == q.x && y == q.y;
+	}
+
+	@Override
+	public int hashCode() { return x.hashCode() ^ y.hashCode(); }
+}
+
+class DeadSquareDetector{
+
+	private static boolean IsSpace(BoardCompact b, int x, int y){
+		int wall_mask = ESpace.WALL.getFlag();
+		return (b.tiles[x][y] & wall_mask) == 0;
+	}
+
+	public static boolean[][] detect(BoardCompact board){
+		int width = board.width();
+		int height = board.height();
+
+		boolean[][] deadTiles = new boolean[width][height];
+
+		for (boolean[] array : deadTiles)
+			Arrays.fill(array, true);
+
+		HashSet<Pair<Integer, Integer>> explored = new HashSet<>();
+		Queue<Pair<Integer, Integer>> frontier = new LinkedList<Pair<Integer, Integer>>();
+
+		for(int x = 0; x < width; x++){
+			for (int y = 0; y < width; y++){
+				int mask = EPlace.SOME_BOX_PLACE_FLAG;
+				if ((board.tiles[x][y] & mask) != 0)
+					frontier.add(new Pair<>(x, y));
+			}
+		}
+
+		while ( ! frontier.isEmpty()) {
+			Pair<Integer, Integer> tile = frontier.poll();
+
+			if (explored.contains(tile))
+				continue;
+
+			deadTiles[tile.x][tile.y] = false;
+
+			for (int dx = -1; dx <= 1; dx++){
+				for (int dy = -1; dy <= 1; dy++){
+
+					if (dx != 0 && dy != 0)
+						continue;
+
+					if (! IsSpace(board, tile.x + dx, tile.y + dy) || ! IsSpace(board, tile.x + dx*2, tile.y + dy*2)){
+						continue;
+					}
+
+					frontier.add(new Pair<>(tile.x + dx, tile.y + dy));
+				}
+			}
+
+			explored.add(tile);
+		}
+
+		return deadTiles;
+	}
+}
 
 /**
  * The simplest Tree-DFS agent.
@@ -17,67 +98,17 @@ public class MyAgent extends ArtificialAgent {
 	
 	@Override
 	protected List<EDirection> think(BoardCompact board) {
-		this.board = board;
-		searchedNodes = 0;
-		long searchStartMillis = System.currentTimeMillis();
-		
-		List<EDirection> result = new ArrayList<EDirection>();
-		dfs(5, result); // the number marks how deep we will search (the longest plan we will consider)
+		this.board = board.clone();
 
-		long searchTime = System.currentTimeMillis() - searchStartMillis;
-        
-        if (verbose) {
-            out.println("Nodes visited: " + searchedNodes);
-            out.printf("Performance: %.1f nodes/sec\n",
-                        ((double)searchedNodes / (double)searchTime * 1000));
-        }
-		
-		return result.isEmpty() ? null : result;
-	}
+		boolean[][] dead = DeadSquareDetector.detect(board);
 
-	private boolean dfs(int level, List<EDirection> result) {
-		if (level <= 0) return false; // DEPTH-LIMITED
-		
-		++searchedNodes;
-		
-		// COLLECT POSSIBLE ACTIONS
-		
-		List<CAction> actions = new ArrayList<CAction>(4);
-		
-		for (CMove move : CMove.getActions()) {
-			if (move.isPossible(board)) {
-				actions.add(move);
+		for(int x = 0; x < board.width(); x ++){
+			for(int y = 0; y < board.width(); y++){
+				System.out.print(dead[x][y] ? '0' : '1');
 			}
+			System.out.println();
 		}
-		for (CPush push : CPush.getActions()) {
-			if (push.isPossible(board)) {
-				actions.add(push);
-			}
-		}
-		
-		// TRY ACTIONS
-		for (CAction action : actions) {
-			// PERFORM THE ACTION
-			result.add(action.getDirection());
-			action.perform(board);
-			
-			// CHECK VICTORY
-			if (board.isVictory()) {
-				// SOLUTION FOUND!
-				return true;
-			}
-			
-			// CONTINUE THE SEARCH
-			if (dfs(level - 1, result)) {
-				// SOLUTION FOUND!
-				return true;
-			}
-			
-			// REVERSE ACTION
-			result.remove(result.size()-1);
-			action.reverse(board);
-		}
-		
-		return false;
+
+		return null;
 	}
 }
